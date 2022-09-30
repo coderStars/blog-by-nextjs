c55416523c5edb3673dce8d721c7b4ad3ee367e3
albert <15935628109@163.com>
2022-09-30 19:16:33 +0800

    feat: 移动端简单适配&删除部分无用css&iconfont使用在线链接方式

--- components/header.js
+++ components/header.js
@@ -1,16 +1,23 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import styles from '../styles/header.module.css';
 
-export default function header({ setGlobalColor }) {
+export default function Header({ setGlobalColor }) {
+    const router = useRouter();
+    const changeRouter = (href) => {
+        router.push(href)
+    }
     return (
         <div className={styles.headerWrap}>
             <div className={styles.headerLeft}>
-                <a className={styles.netName}>coderStar</a>
+                <a className={styles.netName} onClick={e => changeRouter('/')}>coderStar</a>
             </div>
             <ul className={styles.headerRight}>
-                <Link href="/">Me</Link>
-                <Link href="/blog">Blog</Link>
-                <Link href="/project">Projects</Link>
+                <i onClick={e => changeRouter('/blog')} className={styles.mobile + ' iconfont'}>&#xe6c9;</i>
+                <i onClick={e => changeRouter('/project')} className={styles.mobile + ' iconfont'}>&#xe618;</i>
+                <Link href="/"><a className={styles.lapTop}>Me</a></Link>
+                <Link href="/blog" class={styles.lapTop}><a className={styles.lapTop}>Blog</a></Link>
+                <Link href="/project" class={styles.lapTop}><a className={styles.lapTop}>Projects</a></Link>
                 <a href='https://github.com/coderStars' className={styles.iconLink + ' iconfont'}>&#xe885;</a>
                 <a href='https://github.com/coderStars' className={styles.iconLink + ' iconfont'}>&#xe69e;</a>
                 <i className='iconfont' onClick={e => setGlobalColor()}>&#xe632;</i>
